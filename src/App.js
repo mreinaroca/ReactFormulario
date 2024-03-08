27471b48b78762285431148aeacc8d3f4044fd4b
mreinaroca <m.reina@uniandes.edu.co>
2024-03-08 11:37:09 -0500

    Functional State

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import React from 'react';
 function App() {
   // Logic states
   const [formValues, setFormValues] = useState({email:"", password:"", favClass:"1"});
-  const [validationStates, setValidationStates] = useState({ emailState:true, password: true });
+  const validationStates = {emailState: true, passwordState: true};
+  const [validatedForm, setValidatedForm] = useState({emailState: true, passwordState: true});
   // React states
   const [email, setEmail] = React.useState("form-control");
   const [password, setPasswordChange] = React.useState("form-control");
 
   const handleEmailChange = ((e) => {
     setFormValues({...formValues, email: e.target.value});
+    console.log(e.target.value);
+    console.log(validatedForm.emailState);
   });
 
 
   const handlePasswordChange = ((e) => {
     setFormValues({...formValues, password:e.target.value});
+    console.log(e.target.value);
   })
 
   const handleSelectChange = ((e) => {
@@ -27,22 +31,40 @@ function App() {
 
 
   
-  const validateEmail = (email) => {
-    let isValid = email.includes('@') && email.includes('.') && email.length > 5;
-    setValidationStates({...validationStates, emailState: isValid});
+  const validateEmail = () => {
+    let email = formValues.email;
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    let isValid = emailRegex.test(email);
+    validationStates.emailState = isValid;
     setEmail(isValid ? "form-control" : "form-control is-invalid");
+    setValidatedForm({...validatedForm, emailState: isValid});
     return isValid;
   }
 
-  const validatePassword = (password) => {
+  const validatePassword = () => {
+    let password = formValues.password;
     let isValid = password.length >=9 && password.match(/[0-9]/) && password.match(/[a-zA-Z]/);
-    setValidationStates({...validationStates, passwordState: validatePassword(isValid)});
+    validationStates.passwordState = isValid;
     setPasswordChange(isValid ? "form-control" : "form-control is-invalid")
+    setValidatedForm({...validatedForm, passwordState: isValid});
     return isValid;
   }
 
   const validateForm = () => {
-    return validationStates.emailState && validationStates.passwordState;
+    return validationStates.emailState && 
+      validationStates.passwordState &&
+      formValues.email.length > 0 &&
+      formValues.password.length > 0;
+  }
+  
+  const showEmailAsOk = () => {
+    setValidatedForm({...validatedForm, emailState: true});
+    setEmail("form-control");
+  }
+
+  const showPasswordAsOk = () => {
+    setValidatedForm({...validatedForm, passwordState: true});
+    setPasswordChange("form-control");
   }
 
   const clickSubmit= (() => {
@@ -62,26 +84,40 @@ function App() {
           <Form.Label>Email address</Form.Label>
           <Form.Control 
             type="email" 
-            className={email} 
+            className={email} // cambio de color
             placeholder="Enter email" 
             onChange={handleEmailChange} 
-            onFocus={validateEmail(formValues.email)}
+            onFocus={showEmailAsOk}
+            onBlur={validateEmail}
             value={formValues.email}
           />
-          { !validationStates.emailState && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
+          { 
+            !validatedForm.emailState && 
+            <Form.Text className="text-danger">
+              Your email should contain at least one "@" and "." have no spaces and at least 3 characters long
+            </Form.Text>
+          }
+
+
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control 
             type="password" 
-            className={password} 
+            className={password} //cambio de color
             placeholder="Password" 
             onChange={handlePasswordChange} 
-            onFocus={validatePassword}
+            onFocus={showPasswordAsOk}
+            onBlur={validatePassword}
             value={formValues.password}
           />
-          { !validationStates.passwordState && <Form.Text className="text-muted">Your password should have numbers and letters and should be at least 9 characters long</Form.Text>}
+          { 
+            !validatedForm.passwordState && 
+            <Form.Text className="text-danger">
+              Your password should have numbers and letters and should be at least 9 characters long
+            </Form.Text>
+          }
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckBox"/>
@@ -91,7 +127,7 @@ function App() {
             <option value="2">Programación con tecnologías web</option>
           </Form.Select>
         <Form.Group/>
-
+        
         <Button variant="primary" onClick={clickSubmit}>
           Submit
         </Button>

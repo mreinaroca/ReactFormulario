de6a7b9dcd3f90daa62fbbb19e5922515185d634
mreinaroca <m.reina@uniandes.edu.co>
2024-03-08 09:14:18 -0500

    Primer Intento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import React from 'react';
 
 function App() {
+  // Logic states
+  const [formValues, setFormValues] = useState({email:"", password:"", favClass:"1"});
+  const [validationStates, setValidationStates] = useState({ emailState:true, password: true });
+  // React states
+  const [email, setEmail] = React.useState("form-control");
+  const {password, setPassword} = React.useState("form-control");
+
+  const handleEmailChange = ((e) => {
+    setFormValues({...formValues, email: e.target.value});
+    let isValid = validateEmail(e.target.value);
+    setValidationStates({...validationStates, emailState: isValid});
+    setEmail(isValid ? "form-control" : "form-control is-invalid");
+  });
+
+  const handlePasswordChange = ((e) => {
+    setFormValues({...formValues, password:e.target.value});
+    let isValid = validatePassword(e.target.value);
+    setValidationStates({...validationStates, passwordState: validatePassword(isValid)});
+    setPassword(isValid ? "form-control" : "form-control is-invalid");
+  })
+
+  const handleSelectChange = ((e) => {
+    setFormValues({...formValues, favClass: e.target.value});
+  })
+
+  const validateEmail = (email) => {
+    return email.includes('@') && email.includes('.') && email.length > 5;
+  }
+
+  const validatePassword = (password) => {
+    return password.length >=9 && password.match(/[0-9]/) && password.match(/[a-zA-Z]/);
+  }
+
+  const validateForm = () => {
+    return validationStates.emailState && validationStates.passwordState;
+  }
+
+  const clickSubmit= (() => {
+    if(validateForm()){
+      alert("Formulario OK. Datos: "+JSON.stringify(formValues));
+    } else {
+      alert("Formulario con errores. Por favor revise los datos ingresados");
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ejemplo de formularios!</h1>
+      <Form>
+
+        <Form.Group className="mb-6" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" className={email} placeholder="Enter email" onChange={handleEmailChange} value={formValues.email}/>
+          { !validationStates.emailState && <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>}
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange}/>
+          { !validationStates.passwordState && <Form.Text className="text-muted">Your password should have numbers and letters and should be at least 9 characters long</Form.Text>}
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicCheckBox"/>
+          <Form.Label>Favorite Class</Form.Label>
+          <Form.Select onChange={handleSelectChange}>
+            <option value="1">ISIS3710</option>
+            <option value="2">Programación con tecnologías web</option>
+          </Form.Select>
+        <Form.Group/>
+
+        <Button variant="primary" onClick={clickSubmit}>
+          Submit
+        </Button>
+
+      </Form>
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Row, Container, Col } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container>
+      <Row xs={1}>
+        <Col>
+          <App/>
+        </Col>
+      </Row>
+    </Container>
   </React.StrictMode>
 );
 
